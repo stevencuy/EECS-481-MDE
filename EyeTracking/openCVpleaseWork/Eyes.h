0814a9eee612081b77cfdc272d8f14a04a82b44c
Steven Louie <slouie@umich.edu>
2013-11-18 12:59:45 -0500

    eyes variables changed to public

--- EyeTracking/openCVpleaseWork/Eyes.h
+++ EyeTracking/openCVpleaseWork/Eyes.h
@@ -9,6 +9,17 @@ public:
 	Eyes();
 	void findEyes(cv::Mat frame_gray, cv::Rect face);
 
+	int eye_region_width;
+	int eye_region_height;
+	int eye_region_top;
+
+	cv::Rect leftRightCornerRegion;
+	cv::Rect leftLeftCornerRegion;
+	cv::Rect rightLeftCornerRegion;
+	cv::Rect rightRightCornerRegion;
+
+	cv::Point leftPupil, rightPupil;
+
 private:
 	cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow);
 	cv::Point unscalePoint(cv::Point p, cv::Rect origSize);
@@ -17,18 +28,6 @@ private:
 	void testPossibleCentersFormula(int x, int y, unsigned char weight,double gx, double gy, cv::Mat &out);
 	bool floodShouldPushPoint(const cv::Point &np, const cv::Mat &mat);
 	cv::Mat floodKillEdges(cv::Mat &mat);
-
-	int eye_region_width;
-	int eye_region_height;
-	int eye_region_top;
-
-	cv::Rect leftEyeRegion, rightEyeRegion;
-	cv::Rect leftRightCornerRegion;
-	cv::Rect leftLeftCornerRegion;
-	cv::Rect rightLeftCornerRegion;
-	cv::Rect rightRightCornerRegion;
-
-	cv::Point leftPupil, rightPupil;
 };
 
 #endif
